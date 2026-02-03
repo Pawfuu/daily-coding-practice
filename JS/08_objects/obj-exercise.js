9438e88391f62cd43f79374e9e3eade1dca1ef09
Zeya <131510293+Pawfuu@users.noreply.github.com>
2026-02-04 01:33:00 +0800

    08

--- JS/08_objects/obj-exercise.js
+++ JS/08_objects/obj-exercise.js
@@ -181,7 +181,7 @@
 // console.log(users.skillCount());
 // console.log(users.mernStackUsers());
 
-// 3
+// 3-1
 // const personAccount = {
 //   firstName: "Keziah",
 //   lastName: "Joy",
@@ -257,3 +257,164 @@
 // personAccount.addIncome();
 // personAccount.addExpense();
 // console.log(personAccount.accountInfo());
+
+//3-2
+const users = [
+  {
+    _id: "ab12ex",
+    username: "Alex",
+    email: "alex@alex.com",
+    password: "123123",
+    createdAt: "08/01/2020 9:00 AM",
+    isLoggedIn: false,
+  },
+  {
+    _id: "fg12cy",
+    username: "Asab",
+    email: "asab@asab.com",
+    password: "123456",
+    createdAt: "08/01/2020 9:30 AM",
+    isLoggedIn: true,
+  },
+  {
+    _id: "zwf8md",
+    username: "Brook",
+    email: "brook@brook.com",
+    password: "123111",
+    createdAt: "08/01/2020 9:45 AM",
+    isLoggedIn: true,
+  },
+  {
+    _id: "eefamr",
+    username: "Martha",
+    email: "martha@martha.com",
+    password: "123222",
+    createdAt: "08/01/2020 9:50 AM",
+    isLoggedIn: false,
+  },
+  {
+    _id: "ghderc",
+    username: "Thomas",
+    email: "thomas@thomas.com",
+    password: "123333",
+    createdAt: "08/01/2020 10:00 AM",
+    isLoggedIn: false,
+  },
+];
+
+const products = [
+  {
+    _id: "eedfcf",
+    name: "mobile phone",
+    description: "Huawei Honor",
+    price: 200,
+    ratings: [
+      { userId: "fg12cy", rate: 5 },
+      { userId: "zwf8md", rate: 4.5 },
+    ],
+    likes: [],
+  },
+  {
+    _id: "aegfal",
+    name: "Laptop",
+    description: "MacPro: System Darwin",
+    price: 2500,
+    ratings: [],
+    likes: ["fg12cy"],
+  },
+  {
+    _id: "hedfcg",
+    name: "TV",
+    description: "Smart TV:Procaster",
+    price: 400,
+    ratings: [{ userId: "fg12cy", rate: 5 }],
+    likes: ["fg12cy"],
+  },
+];
+
+// a
+// function newUser() {
+//   let name = prompt("Enter the user's name: ");
+//   let user_email = prompt("Enter your email: ");
+//   let password = prompt("Enter the user's password: ");
+
+//   let userExist = users.some((users) => users.email == user_email);
+
+//   if (userExist) {
+//     return alert("Registration Failed! A user with this email already exist.");
+//   } else {
+//     const formattedUser = {
+//       _id: Math.random().toString(36).substring(2, 8),
+//       username: name,
+//       email: user_email,
+//       password: password,
+//       createdAt: new Date().toLocaleString(),
+//       isLoggedIn: false,
+//     };
+//     users.push(formattedUser);
+//     return alert("SUCCESS! Account Created.");
+//   }
+// }
+// newUser();
+
+// b
+// function signIn() {
+//   let name = prompt("Enter the user's name: ");
+//   let password = prompt("Enter the user's password: ");
+
+//   const foundUser = users.find((users) => users.username == name);
+
+//   if (foundUser) {
+//     if (foundUser.password == password) {
+//       alert(`Welcome back, ${foundUser.username}!`);
+//       foundUser.isLoggedIn = true;
+//     } else {
+//       alert("Try again. Wrong password");
+//     }
+//   } else {
+//     alert("User not found.");
+//   }
+// }
+// signIn();
+
+// 3
+// a
+// const rateProduct = () => {
+//   let total_rating = 0;
+//   for (const product of products) {
+//     for (const ratingRecord of product.ratings) {
+//       total_rating += ratingRecord.rate;
+//     }
+//   }
+//   return total_rating;
+// };
+// console.log("Total Rating: ", rateProduct());
+
+// // b
+// function averageRating() {
+//   let total_sum = rateProduct();
+//   let final_rate = total_sum / products.length;
+//   return final_rate;
+// }
+// console.log("Average Rating: ", averageRating());
+
+//4
+const likeProduct = (productID, userID) => {
+  const product = products.find((products) => products._id == productID);
+
+  if (product) {
+    const alreadyLiked = product.likes.includes(userID);
+
+    if (alreadyLiked) {
+      product.likes = product.likes.filter((id) => id !== userID);
+      console.log(`Unlike successful for product: ${product.name}`);
+    } else {
+      product.likes.push(userID);
+      console.log(`Added to Like for product: ${product.name}`);
+    }
+  } else {
+    console.log("Product not found!");
+  }
+};
+likeProduct("ab12ex", "fg12cy");
+likeProduct("hedfcg", "fg12cy");
